69fc37c21a02cb184c67e4461d4d06b7d49ea995
EL Gamer <keineleguia1975@gmail.com>
2025-02-05 23:09:38 -0500

    Actualizar

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []; 
+let mostrarNombre = 0;
 
 
 function agregarAmigo(){
-    let listaAmigos = document.getElementById('amigo').value;
-    if (listaAmigos == ''){
+    let listAmigos = document.getElementById('amigo').value;
+
+    if (listAmigos == ''){
         alert('Por favor, inserte un nombre.')
     }else{
         //agreagar lista de amigos.
-        amigos.push(listaAmigos);
+        amigos.push(listAmigos);
+        
         let lista = document.getElementById('listaAmigos');
         lista.innerHTML = '';
         
-        for (let i = 1; i < amigos.length; i++) {
+        for (let i = 0; i < amigos.length; i++) {
             let lit = document.createElement('li');
             lit.textContent = amigos[i];
             lista.appendChild(lit);
+            
         }
+       
     }
 }
 
 function sortearAmigo(){
     let generarAmigoAle = Math.floor(Math.random()* amigos.length)+1;
-    if (generarAmigoAle == amigos[i]){
-        console.log('dadasdj')
+    if (generarAmigoAle == 2){
+        mostrarNombre = document.getElementById("Bienev"+'resultado');
+        mostrarNombre.innerHTML = '';
+        
     }
-
+    console.log(generarAmigoAle)
+    
+    
 }
+
 sortearAmigo();
